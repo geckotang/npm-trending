4bd13c11d4dad08f4b4868d43b9b4b6226f9a615
Daijiro Wachi <daijiro.wachi@gmail.com>
2016-03-06 02:30:28 +0100

    minor tweaks to css

--- app/index.js
+++ app/index.js
@@ -1,4 +1,4 @@
- var fs = require('fs')
+var fs = require('fs')
 var path = require('path')
 var express = require('express')
 var render = require('ejs').render
@@ -19,7 +19,7 @@ app.get('/', function (req, res) {
 
 app.get('/page/:number', function (req, res) {
   var page = req.params.number
-  var hasOnlyNumber =  /^[0-9]*$/.test(page)
+  var hasOnlyNumber = /^[0-9]*$/.test(page)
   if (!hasOnlyNumber) res.status(404).send('/page/:number can has only Number')
   var trend = getTrending(page)
   if (Object.keys(trend).length === 0) res.status(418).send("I'm a teapot.")
